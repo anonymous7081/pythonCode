5e0d6f78e63655340b58dd3a20dfee1390981405
anonymous7081 <yadit7791@gmail.com>
2025-09-22 18:00:53 +0530

    new1

--- sum.py
+++ sum.py
@@ -1,65 +1,65 @@
 
 
-principal = int (input("Enter the principal amount: "))
-rate = float(input("Enter the rate of interest: "))
-time = int(input("Enter the time in years: "))
+# principal = int (input("Enter the principal amount: "))
+# rate = float(input("Enter the rate of interest: "))
+# time = int(input("Enter the time in years: "))
 
-simple_interest = (principal * rate * time) / 100
+# simple_interest = (principal * rate * time) / 100
 
-compound_intrest = principal * ((1 + (rate / 100)) ** time - 1)
-
-
-total_amount = principal + simple_interest
-print("The simple interest is:", simple_interest)
-print("The compound interest is:", compound_intrest)
-
-first = int(input("Enter the first number: "))
-
-cube = first ** 3
-square = first ** 2
-difference = cube - square
-print("the difference between the cube and square of the number is:", difference)
-a = int (input("Enter the first number: "))
-b = int(input("Enter the second number: "))
-c= a/b
-print(c)
-print ("integer =", int(c))
-print ("decimal =",c-int(c))
-
-x = int(input("Enter the first number: "))
-last_digit= x%10
-print("The last digit of the number is:", last_digit)
-
-s = "ramesh"
-
-# s[0]  = "p"
-print(s[0])
-s = "bansal"
-s1 = ""
-for t in s:
-    if t =="a":
-        pass
-    elif t != "a":
+# compound_intrest = principal * ((1 + (rate / 100)) ** time - 1)
 
 
-# t = "a"
-# t = t+ "b"
+# total_amount = principal + simple_interest
+# print("The simple interest is:", simple_interest)
+# print("The compound interest is:", compound_intrest)
 
-# print(t)
-# f = True
-# str = "ram     is    dancing    on   the  floor"
+# first = int(input("Enter the first number: "))
 
-# str = str + " "
-# word=""
-# for t in str:
-#     if t != " ":
-#         word = t + word
-#     elif t == " " and word !="":
-#         print(word)
-#     print(word)
+# cube = first ** 3
+# square = first ** 2
+# difference = cube - square
+# print("the difference between the cube and square of the number is:", difference)
+# a = int (input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# c= a/b
+# print(c)
+# print ("integer =", int(c))
+# print ("decimal =",c-int(c))
 
-# str = "Mukesh Kumar Ambani"
+# x = int(input("Enter the first number: "))
+# last_digit= x%10
+# # print("The last digit of the number is:", last_digit)
 
-# for t in str:
-#     if t != " ":
-#         print(t(1).upper(),".")
+# s = "ramesh"
+
+# # s[0]  = "p"
+# print(s[0])
+# s = "bansal"
+# s1 = ""
+# for t in s:
+#     if t =="a":
+#         pass
+#     elif t != "a":
+
+
+# # t = "a"
+# # t = t+ "b"
+
+# # print(t)
+# # f = True
+# # str = "ram     is    dancing    on   the  floor"
+
+# # str = str + " "
+# # word=""
+# # for t in str:
+# #     if t != " ":
+# #         word = t + word
+# #     elif t == " " and word !="":
+# #         print(word)
+# #     print(word)
+
+# # str = "Mukesh Kumar Ambani"
+
+# # for t in str:
+# #     if t != " ":
+# #         print(t(1).upper(),".")
